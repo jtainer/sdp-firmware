d396ad527ae90125b3ae3b1e7b8812c8133224e8
jtainer <jonathantainer@yahoo.com>
2024-02-20 06:22:38 -0500

    Create IMU test program

--- lib/entry/entry.c
+++ lib/entry/entry.c
@@ -6,14 +6,53 @@
 // Copyright (c) 2024 Jonathan Tainer
 //
 
+#include <main.h>
 #include <stdio.h>
-#include "main.h"
+#include <i2c-mux.h>
+#include <Library/w25q_mem.h>
+#include <bno055_stm32.h>
+
+extern I2C_HandleTypeDef hi2c1;
+const int IMU_COUNT = 8;
+
+i2c_mux_t mux;
+
 
 void setup(void) {
+	// Set mux 0 address offset to 0
+	HAL_GPIO_WritePin(MUX0_A0_GPIO_Port, MUX0_A0_Pin, 0);
+	HAL_GPIO_WritePin(MUX0_A1_GPIO_Port, MUX0_A1_Pin, 0);
+	HAL_GPIO_WritePin(MUX0_A2_GPIO_Port, MUX0_A2_Pin, 0);
+	
+	// Set mux 1 address offset to 7
+	HAL_GPIO_WritePin(MUX1_A0_GPIO_Port, MUX1_A0_Pin, 1);
+	HAL_GPIO_WritePin(MUX1_A1_GPIO_Port, MUX1_A1_Pin, 1);
+	HAL_GPIO_WritePin(MUX1_A2_GPIO_Port, MUX1_A2_Pin, 1);
 
+
+	// Setup multiplexer
+	mux.hi2c = &hi2c1;
+	mux.rst_port = MUX0_RST_GPIO_Port;
+	mux.rst_pin = MUX0_RST_Pin;
+	mux.addr_offset = 0;
+	i2c_mux_reset(&mux);
+	
+	bno055_assignI2C(&hi2c1);
+
+	for (int i = 0; i < IMU_COUNT; i++) {
+		printf("Checking IMU[%d]...\r\n", i);
+		i2c_mux_select(&mux, i);
+		bno055_setup();
+		bno055_setOperationModeNDOF();
+	}
 }
 
 void loop(void) {
-	printf("Hello world\r\n");
+	for (int i = 0; i < IMU_COUNT; i++) {
+		i2c_mux_select(&mux, i);
+		bno055_vector_t v = bno055_getVectorQuaternion();
+		printf("IMU[%d]: w=%f,\tx=%f,\ty=%f,\tz=%f\r\n", i, v.w, v.x, v.y, v.z);
+	}
+
 	HAL_Delay(1000);
 }
