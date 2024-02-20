582a4dd614ef3731730437a3a638282770b1ebc6
jtainer <jonathantainer@yahoo.com>
2024-02-20 06:21:07 -0500

    Add i2c.h

---
+++ inc/i2c.h
@@ -0,0 +1,16 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Feb 20, 2024
+ *      Author: jtain
+ */
+
+#ifndef INC_I2C_H_
+#define INC_I2C_H_
+
+#include "stm32h7xx_hal.h"
+
+extern I2C_HandleTypeDef hi2c1;
+extern I2C_HandleTypeDef hi2c2;
+
+#endif /* INC_I2C_H_ */
