3c19c741f72ad007c05f430cf33dafbc51c7a6b3
jtainer <jonathantainer@yahoo.com>
2024-02-20 19:57:48 -0500

    Modify _write() to block until USB port is available to transmit

--- src/main.c
+++ src/main.c
@@ -468,6 +468,11 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 // Override _write definition so stdout is directed to USB
 int _write(int file, char* ptr, int len) {
+	// Block until USB port is available for transmitting
+	extern USBD_HandleTypeDef hUsbDeviceFS;
+	USBD_CDC_HandleTypeDef* hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
+	while (hcdc->TxState != 0) asm("nop");
+	// Begin transmission
 	CDC_Transmit_FS((uint8_t*) ptr, len);
 	return len;
 }
